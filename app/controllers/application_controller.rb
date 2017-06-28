4852bfef9bbebb9467897628958f4e2714defb36
Charles Pernet <charlespernet@gmail.com>
2017-06-28 10:56:47 +0200

    secure staging with http_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.staging?
+    http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+  end
+  
   protect_from_forgery with: :exception
 end

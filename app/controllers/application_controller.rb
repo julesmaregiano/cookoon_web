6c99c01ce6bce1150a3d51ed96425ced7cee4f3c
Charles Pernet <charlespernet@gmail.com>
2017-07-03 14:58:03 +0200

    Custom Forms + Updated devicse to allow Users to edit profile without password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :description])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :description])
   end
 end

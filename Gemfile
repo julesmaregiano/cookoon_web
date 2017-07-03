1981411e5ae3fde2892958c51425bfef95d740f4
Charles Pernet <charlespernet@gmail.com>
2017-07-03 12:57:45 +0200

    Install Devise + Quick Custom for Views + Migration to add few fields to Users

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jquery-rails'
 
 gem 'figaro'
 
+gem 'devise'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   if Rails.env.staging?
     http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
   end
-  
-  protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :description])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end

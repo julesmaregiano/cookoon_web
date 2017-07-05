9c03ca23d8fc8b4288731b5cc826ac2fe3f688e0
Charles Pernet <charlespernet@gmail.com>
2017-07-05 13:29:53 +0200

    Add monetize to Reservation
    - add validation on price presence
    - defin .cookoon_fee to save in one place our fees

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :cookoon
   belongs_to :user
+
+  monetize :price_cents
+
+  validates :price_cents, presence: true
+
+  def self.cookoon_fee
+    0.07
+  end
 end

dc9b983640844b802a1682a6c5d80aa71688a813
Charles Pernet <charlespernet@gmail.com>
2017-07-15 17:16:45 +0200

    Add authenticated root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Different root for authenticated users
+  authenticated do
+    root 'cookoons#index'
+  end
   root 'pages#home'
 
   resources :cookoons do

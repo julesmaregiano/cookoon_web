c2c08c07d5f4181125c61c586782ea81eed2c3c1
Charles Pernet <charlespernet@gmail.com>
2017-07-18 12:46:58 +0200

    Make sure that user see Cookoons even when first search

--- app/controllers/cookoons_controller.rb
+++ app/controllers/cookoons_controller.rb
@@ -2,16 +2,9 @@ class CookoonsController < ApplicationController
   def index
     @new_search = UserSearch.new(number: 1, duration: 1)
     @last_search = current_user.user_searches.last || @new_search
-    @cookoons = Cookoon.near(@last_search.address, 10)
-    @search_infos = {
-      position: @last_search.address || 'Autour de vous',
-      time_slot: @last_search.datetime.try(:strftime, '%a %d - %H:%M') || 'Tout de suite',
-      people_number: @last_search.number || 4
-    }
-    @hash = Gmaps4rails.build_markers(@cookoons) do |cookoon, marker|
-      marker.lat cookoon.latitude
-      marker.lng cookoon.longitude
-    end
+    @cookoons = Cookoon.near(@last_search.address || 'Paris' , 10)
+    @search_infos = prepare_infos
+    @hash = build_markers
   end
 
   def new
@@ -36,5 +29,20 @@ class CookoonsController < ApplicationController
 
   def cookoon_params
     params.require(:cookoon).permit(:name, :surface, :price, :address, :capacity, :category, photos: [])
+  end
+
+  def prepare_infos
+    {
+      position: @last_search.address || 'Autour de vous',
+      time_slot: @last_search.datetime.try(:strftime, '%a %d - %H:%M') || 'Tout de suite',
+      people_number: @last_search.number || 4
+    }
+  end
+
+  def build_markers
+    Gmaps4rails.build_markers(@cookoons) do |cookoon, marker|
+      marker.lat cookoon.latitude
+      marker.lng cookoon.longitude
+    end
   end
 end

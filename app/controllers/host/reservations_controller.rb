afa1f9b3ccf81e408dc95b0190a0281592db2a46
Charles Pernet <charlespernet@gmail.com>
2017-07-26 16:08:23 +0200

    Add style for Host::Reservation#edit view

--- app/controllers/host/reservations_controller.rb
+++ app/controllers/host/reservations_controller.rb
@@ -1,14 +1,21 @@
 class Host::ReservationsController < ApplicationController
+  before_action :find_reservation, only: [:edit, :update]
+
   def index
     @reservations = current_user.reservations_requests
     @cookoons = current_user.cookoons
   end
 
   def edit
-    @reservation = Reservation.find(params[:id])
   end
 
   def update
     byebug
+  end
+
+  private
+
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
   end
 end

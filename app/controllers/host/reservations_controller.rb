23fede5451abcd9b5e55adf0ac4c39130a06e969
Charles Pernet <charlespernet@gmail.com>
2017-07-26 16:37:47 +0200

    Add Host:Reservation#update

--- app/controllers/host/reservations_controller.rb
+++ app/controllers/host/reservations_controller.rb
@@ -10,12 +10,25 @@ class Host::ReservationsController < ApplicationController
   end
 
   def update
-    byebug
+    status = params["accept"] ? :accepted : :refused
+    displayable_status = status == :accepted ? "accepté" : "refusé"
+    merged_params = reservation_params.merge(status: status)
+    if @reservation.update(merged_params)
+      flash[:notice] = "Vous avez #{displayable_status} la réservation"
+      redirect_to host_reservations_path
+    else
+      flash[:alert] = 'Erreur'
+      render :edit
+    end
   end
 
   private
 
   def find_reservation
     @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:cleaning, :janitor)
   end
 end

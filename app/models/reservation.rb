e3eb9cf523b9c3fcc1ca3bfabf8de86057c33896
Charles Pernet <charlespernet@gmail.com>
2017-08-02 15:51:31 +0200

    Add has_one :inventory to Reservation, and 2 statuses :ongoing and :passed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,13 @@
 class Reservation < ApplicationRecord
   belongs_to :cookoon
   belongs_to :user
+  has_one :inventory
 
   monetize :price_cents
 
   validates :price_cents, presence: true
 
-  enum status: [ :pending, :accepted, :refused, :paid, :cancelled ]
+  enum status: [ :pending, :accepted, :refused, :paid, :cancelled, :ongoing, :passed ]
 
   def cookoon_fee_rate
     0.07

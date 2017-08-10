4723f8ed1c40f87d458d34569b04981004a0146a
Charles Pernet <charlespernet@gmail.com>
2017-08-10 12:22:23 +0200

    Wip on stripe infos collect

--- app/controllers/stripe_accounts_controller.rb
+++ app/controllers/stripe_accounts_controller.rb
@@ -1,4 +1,15 @@
+require 'stripe_account_wrapper'
+
 class StripeAccountsController < ApplicationController
   def new
+  end
+
+  def create
+    account_wrapper = StripeAccountWrapper.new(params: params[:stripe], user: current_user, request_ip: request.remote_ip)
+    # account = account_wrapper.retrieve_or_create_current_user_account
+    byebug
+    # account_wrapper.enrich(account)
+
+    render :new
   end
 end

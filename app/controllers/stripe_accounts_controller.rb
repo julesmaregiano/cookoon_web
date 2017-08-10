66bab416e5c119b897f17848dba3d5508cc89079
Charles Pernet <charlespernet@gmail.com>
2017-08-10 17:37:34 +0200

    Stripe connect account working as external lib

--- app/controllers/stripe_accounts_controller.rb
+++ app/controllers/stripe_accounts_controller.rb
@@ -6,10 +6,22 @@ class StripeAccountsController < ApplicationController
 
   def create
     account_wrapper = StripeAccountWrapper.new(params: params[:stripe], user: current_user, request_ip: request.remote_ip)
-    # account = account_wrapper.retrieve_or_create_current_user_account
-    byebug
-    # account_wrapper.enrich(account)
 
-    render :new
+    if account_wrapper.errors.any?
+      flash[:alert] = account_wrapper.errors.join(", ")
+      return redirect_to new_stripe_account_path
+    end
+
+    byebug
+
+    account = account_wrapper.retrieve_or_create_user_account
+
+    if account && account_wrapper.enrich(account)
+      flash[:notice] = "Votre compte est maintenant lié avec Stripe. Vous pourrez prochainement récupérer vos paiements en attente"
+      redirect_to edit_user_registration_path
+    else
+      flash[:alert] = "Une erreur est survenue avec notre partenaire de paiement veuillez retenter ultérieurement, pour des raisons de sécurité nous ne conservons pas vos données bancaires veuillez les saisir à nouveau."
+      redirect_to new_stripe_account_path
+    end
   end
 end
--- lib/stripe_account_wrapper.rb
+++ lib/stripe_account_wrapper.rb
@@ -1,56 +1,105 @@
-require 'byebug'
-
 class StripeAccountWrapper
-  attr_reader :params, :user, :request_ip
+
+  attr_reader :params, :user, :request_ip, :account, :errors
 
   def initialize(attributes)
     @params = attributes[:params]
     @user = attributes[:user]
     @request_ip = attributes[:request_ip]
+    check_params
   end
 
-  def retrieve_or_create_current_user_account
+  def retrieve_or_create_user_account
     if user.stripe_account_id.nil?
       # create stripe account via api
-      account = Stripe::Account.create(
-        type: 'custom',
-	      country: 'FR',
-  	    email: user.email,
-	      legal_entity: {
-	        first_name: user.first_name,
-	        last_name: user.last_name,
-	        type: "individual"
-	      },
-	      tos_acceptance: {
-		      date: DateTime.now.to_i,
-		      ip: request_ip
-	      }
-      )
+      account = create_stripe_account
 
-      user.update(stripe_account_id: account.id)
+      user.update(stripe_account_id: account.id) if account
     else
-      # retriever from api
-      Stripe::Account.retrieve(user.stripe_account_id)
+      # retrieve from api
+      account = Stripe::Account.retrieve(user.stripe_account_id)
     end
+
+    account
   end
 
   def enrich(account)
-    account.external_account = {
-    	object: "bank_account",
-    	account_holder_name: user.full_name,
-    	currency: "eur",
-    	country: "FR",
-    	account_holder_type: "individual",
-    	account_number: params[:iban]
-    }
+    return false unless account
 
-    account.legal_entity.address.city = params[:address][:city]
-    account.legal_entity.address.line1 = params[:address][:street]
-    account.legal_entity.address.postal_code =  params[:address][:post_code]
-    account.legal_entity.dob.day = params["dob(3i)"].to_i
-    account.legal_entity.dob.month = params["dob(2i)"].to_i
-    account.legal_entity.dob.year = params["dob(1i)"].to_i
+    begin
+      account.external_account = {
+      	object: "bank_account",
+      	account_holder_name: user.full_name,
+      	currency: "eur",
+      	country: "FR",
+      	account_holder_type: "individual",
+      	account_number: params[:iban]
+      }
 
-    account.save
+      account.legal_entity.address.city = params[:address][:city]
+      account.legal_entity.address.line1 = params[:address][:street]
+      account.legal_entity.address.postal_code = params[:address][:post_code]
+      account.legal_entity.dob.day = params["dob(3i)"].to_i
+      account.legal_entity.dob.month = params["dob(2i)"].to_i
+      account.legal_entity.dob.year = params["dob(1i)"].to_i
+
+      account.save
+    rescue Stripe::InvalidRequestError => e
+      Rails.logger.error("Faild to enrich Stripe account")
+      Rails.logger.error(e.message)
+      false
+    end
+  end
+
+  private
+
+  def create_stripe_account
+    begin
+      Stripe::Account.create(
+        type: 'custom',
+        country: 'FR',
+        email: user.email,
+        legal_entity: {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          type: "individual"
+        },
+        tos_acceptance: {
+          date: DateTime.now.to_i,
+          ip: request_ip
+        }
+      )
+    rescue Stripe::InvalidRequestError => e
+      Rails.logger.error("Faild to create Stripe account")
+      Rails.logger.error(e.message)
+      false
+    end
+  end
+
+  def retrieve_stripe_account
+    begin
+      Stripe::Account.retrieve(user.stripe_account_id)
+    rescue Stripe::PermissionError => e
+      Rails.logger.error("Faild to retrieve Stripe account")
+      Rails.logger.error(e.message)
+      false
+    end
+  end
+
+  def check_params
+    @errors = []
+    address_params = [params["address"]["city"], params["address"]["street"], params["address"]["post_code"]]
+    if address_params.any? { |param| !param.present? }
+      @errors << "Votre adresse est obligatoire"
+    end
+
+    dob_params = [params["dob(1i)"], params["dob(2i)"], params["dob(2i)"]]
+    if dob_params.any? { |param| !param.present? }
+      @errors << "Votre date de naissance est obligatoire"
+    end
+
+    if !params["iban"].present?
+      @errors << "Votre IBAN est obligatoire"
+    end
   end
 end

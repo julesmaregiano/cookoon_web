5dcec6537063b26f75946d06c8cd8624af597817
Charles Pernet <charlespernet@gmail.com>
2017-08-15 16:05:22 +0200

    Rename host_price_computing.js to price_computing.js + add more logic

---
+++ app/assets/javascripts/price_computing.js
@@ -0,0 +1,27 @@
+$('.reservation-option').change(function() {
+  compute_price_for_host($(this));
+})
+
+$('#reservation_duration').change(function() {
+  compute_price_for_rent($(this));
+});
+
+function compute_price_for_host(input) {
+  var displayPrice = parseFloat($('#display-price').text());
+  var optionPrice = input.data('price');
+  if(input.is(":checked")) {
+    $('#display-price').text(displayPrice - optionPrice);
+  }
+  else {
+    $('#display-price').text(displayPrice + optionPrice);
+  }
+}
+
+function compute_price_for_rent(duration_input) {
+  var price_cents_without_fees = ($('#cookoon-price').text() * duration_input.val()) * 100;
+  var total_price = (price_cents_without_fees * 1.05) / 100
+  $('#total-price').text(total_price);
+}
+
+
+// @reservation.rent_cookoon_fee_rate

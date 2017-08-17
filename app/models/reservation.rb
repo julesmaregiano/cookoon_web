e5b5532fcf2473e64a30ff54f2d04d2d2b4ab7af
Charles Pernet <charlespernet@gmail.com>
2017-08-17 17:14:40 +0200

    Add bunch of methods to compute prices for Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,11 +10,11 @@ class Reservation < ApplicationRecord
   enum status: [ :pending, :accepted, :refused, :paid, :cancelled, :ongoing, :passed ]
 
   def host_cookoon_fee_rate
-    0.07
+    0.05
   end
 
   def rent_cookoon_fee_rate
-    0.05
+    0.07
   end
 
   def pending_or_paid?
@@ -29,7 +29,32 @@ class Reservation < ApplicationRecord
     price_for_rent * rent_cookoon_fee_rate
   end
 
+  def host_cookoon_fees
+    price_for_rent * host_cookoon_fee_rate
+  end
+
   def price_for_rent_with_fees
     price_for_rent + cookoon_fees
+  end
+
+  def price_for_services
+    amount = 0
+    amount += base_option_price if janitor
+    amount += base_option_price if cleaning
+    Money.new amount * 100
+  end
+
+  def payout_price_for_host
+    price_for_rent - total_fees_with_services_for_host
+  end
+
+  def total_fees_with_services_for_host
+    host_cookoon_fees + price_for_services
+  end
+
+  private
+
+  def base_option_price
+    15
   end
 end

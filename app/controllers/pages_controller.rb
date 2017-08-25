a45a5e5f4093cd4a2dc30dd80178df1ffb081589
Charles Pernet <charlespernet@gmail.com>
2017-08-25 11:47:09 +0200

    Add method to set cookies via Rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :setcookies]
   layout "naked"
 
   def home
+  end
+
+  def setcookies
+    lat_lng = "#{params[:lat]}|#{params[:lng]}"
+    cookies[:lat_lng] ||= { value: lat_lng, expires: 5.hour.from_now }
   end
 end

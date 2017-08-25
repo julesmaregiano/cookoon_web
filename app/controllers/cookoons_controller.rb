eefa706a050d85c95c2322f8ebe4c3714a933972
Charles Pernet <charlespernet@gmail.com>
2017-08-25 11:52:58 +0200

    Clean before merge

--- app/controllers/cookoons_controller.rb
+++ app/controllers/cookoons_controller.rb
@@ -3,7 +3,7 @@ class CookoonsController < ApplicationController
     @lat_lng = cookies[:lat_lng].try(:split, "|")
     @new_search = UserSearch.new(number: 2, duration: 2, date: DateTime.now + 2.days)
     @last_search = current_search || @new_search
-    @cookoons = Cookoon.near( @last_search.address.presence || @lat_lng || 'Paris', 10)
+    @cookoons = Cookoon.near(@last_search.address.presence || @lat_lng || 'Paris', 10)
     prepare_infos
     build_markers
   end

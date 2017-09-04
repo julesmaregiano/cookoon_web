7d01edf1f07a61810ac75f09e03feccc0b4cd1b9
Charles Pernet <charlespernet@gmail.com>
2017-09-04 14:55:16 +0200

    Overlay when sidenav is opened

--- app/assets/javascripts/side_nav.js
+++ app/assets/javascripts/side_nav.js
@@ -1,7 +1,9 @@
 $('#side-nav-clicker').click(function() {
-  $(".side-nav").show("slide", { direction: "left" }, 500);
+  $('.overlay').fadeIn();
+  $('.side-nav').show('slide', { direction: 'left' }, 500);
 })
 
 $('#side-nav-closer').click(function() {
-  $(".side-nav").hide("slide", { direction: "left" }, 500);
+  $('.overlay').fadeOut();
+  $('.side-nav').hide('slide', { direction: 'left' }, 500);
 })

d2c61c8acd2973299d8495c4b52a4f2f979878ec
Charles Pernet <charlespernet@gmail.com>
2017-09-28 14:22:38 +0200

    Configure Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   if Rails.env.staging?
     http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
@@ -19,5 +24,11 @@ class ApplicationController < ActionController::Base
   # disable devises flashes
   def is_flashing_format?
     false
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
